cd177ca7c008d0fe6d74b7efc35345c3389fb2d4
Kiran Pande <kiranpande1987@gmail.com>
2021-09-29 14:49:39 +0530

    Added cities.json for City List.

--- app/src/main/java/com/kprights/weatherapp/MainActivity.kt
+++ app/src/main/java/com/kprights/weatherapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.kprights.weatherapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.AssetManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import com.kprights.weatherapp.model.Base
+import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getCitiesAndCountriesFromAssets()
+    }
+
+    private fun getCitiesAndCountriesFromAssets() {
+        val json = this.assets.readAssetsFile("cities.json")
+
+        sampletext.text = "Timber"
+
+        val gson = Gson()
+        val m: Base = gson.fromJson(json, Base::class.java)
+        sampletext.text = m.msg
+        sampletext.text = m.data?.get(0)?.country
+
+        // Timber.e(json)
     }
 }
+
+fun AssetManager.readAssetsFile(fileName: String): String = open(fileName).bufferedReader().use{it.readText()}
---
+++ app/src/main/java/com/kprights/weatherapp/view/CityListFragment.kt
@@ -0,0 +1,15 @@
+package com.kprights.weatherapp.view
+
+import androidx.fragment.app.Fragment
+
+
+/**
+ * Copyright (c) 2021 for KPrights
+ *
+ * User : Kiran Pande
+ * Date : 29/9/21
+ * Time : 12:46 PM
+ */
+
+class CityListFragment: Fragment() {
+}
