8e97cd5dda6971bf66d9571a70f8aae338552471
Kiran Pande <kiranpande1987@gmail.com>
2021-10-02 19:53:27 +0530

    Code cleanup, removed unwanted classes.

--- app/src/main/java/com/kprights/weatherapp/common/BindingAdapter.kt
+++ app/src/main/java/com/kprights/weatherapp/common/BindingAdapter.kt
@@ -1,18 +1,14 @@
 package com.kprights.weatherapp.common
 
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.kprights.weatherapp.R
-import com.kprights.weatherapp.view.CityListAdapter
 import com.kprights.weatherapp.viewmodel.ApiStatus
-import java.text.SimpleDateFormat
 import java.util.*
 
 
@@ -23,12 +19,6 @@ import java.util.*
  * Date : 29/9/21
  * Time : 4:25 PM
  */
-
-@BindingAdapter("listData")
-fun bindRecyclerView(recyclerView: RecyclerView, data: List<String>) {
-    val adapter = recyclerView.adapter as CityListAdapter
-    adapter.submitList(data)
-}
 
 @BindingAdapter("showImage")
 fun showImage(imageViewForNewsFeed: ImageView, icon: String?) {
